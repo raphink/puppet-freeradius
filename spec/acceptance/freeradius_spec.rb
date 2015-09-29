cda099eeea85856ae590eb1a9ddd4f178beaf780
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-29 15:21:14 +0200

    Add freeradius_spec.rb

---
+++ spec/acceptance/freeradius_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper_acceptance'
+
+describe 'freeradius' do
+  context 'when running puppet code' do
+    pp = "class { 'freeradius': }"
+
+    it 'should apply with no errors' do
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    it 'should converge on the first run' do
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+end
