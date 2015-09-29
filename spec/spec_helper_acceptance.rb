e83f94159aa447aa94dbf97df77eedf428d627cb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-29 15:04:22 +0200

    Add spec_helper_acceptance.rb

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,17 @@
+require 'beaker-rspec'
+
+install_puppet_on hosts
+
+RSpec.configure do |c|
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_name = module_root.split('-').last
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module
+    puppet_module_install(:source => module_root, :module_name => module_name)
+  end
+end
