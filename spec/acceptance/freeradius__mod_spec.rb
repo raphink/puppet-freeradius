20e49951eda94c974f9ae1312a9e6e63c8eaa7a9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-11-09 15:25:48 +0100

    Add acceptance tests for freeradius::mod

--- spec/acceptance/freeradius__mod_spec.rb
+++ spec/acceptance/freeradius__mod_spec.rb
@@ -20,5 +20,13 @@ describe 'freeradius::mod' do
     it 'should converge on the first run' do
       apply_manifest(pp, :catch_changes => true)
     end
+
+    describe file('/etc/raddb/mods-enabled/perl') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_owned_by('root') }
+      it { is_expected.to be_grouped_into('radiusd') }
+      it { is_expected.to be_mode('0640') }
+      its(:content) { is_expected.to match(/auth\.pl$/) }
+    end
   end
 end
