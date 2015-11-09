3b3f53fcda3b00c73ee05fbd756969890fee190d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-11-09 15:26:35 +0100

    Add code for freeradius::mod

--- manifests/mod.pp
+++ manifests/mod.pp
@@ -2,4 +2,18 @@ define freeradius::mod (
   $ensure = 'present',
   $content = undef,
 ) {
+  if ($ensure == 'present') {
+    validate_re($content, '\S+', 'Must pass content to freeradius::mod')
+    $file_ensure = 'file'
+  } else {
+    $file_ensure = $ensure
+  }
+
+  file { "/etc/raddb/mods-enabled/${name}":
+    ensure  => $file_ensure,
+    owner   => 'root',
+    group   => 'radiusd',
+    mode    => '0640',
+    content => $content,
+  }
 }
